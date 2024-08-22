b6090f0a816cb815d7844294020e7f3870eeea55
Aryan <aryangandhi24@gmail.com>
2024-08-22 14:44:20 -0400

    setup file created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Machine Learning Chatbot',
+    version= '0.0.0',
+    author= 'Aryan Gandhi',
+    packages= find_packages(),
+    install_requires = []
+
+)
